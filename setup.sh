4ae1b16b0164a71da094ca91a5c0b4661c18bed3
Fred-ITUX <thealldedfred@gmail.com>
2025-05-02 13:34:16 +0200

    patch update

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+git config --global user.email "thealldedfred@gmail.com"                                      
+git config --global user.name "Fred-ITUX"
+
+
+find ~/ -type f -name "*.sh" -exec chmod +x {} +
+
