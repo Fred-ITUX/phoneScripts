fb65ab21aeeeea5909a8d475e7edb12db24ed172
Fred-ITUX <thealldedfred@gmail.com>
2025-05-02 14:05:15 +0200

    patch update

--- Utilities/profile.sh
+++ Utilities/profile.sh
@@ -10,6 +10,8 @@ utilitiesPath="$HOME/phoneScripts/Utilities"
 
 #### Quality of life
 alias addx='chmod +x'
+alias addexec='find ~/phoneScripts -type f -name "*.sh" -exec chmod +x {} +'
+
 alias cd="cd $HOME"
 alias c='clear'
 alias e='exit'
@@ -25,11 +27,11 @@ alias setup="$utilitiesPath/setup.sh"
 
 
 #### Python scripts
-alias converterMetric="$scriptsPath/measure_unit_converter.py"
-alias pswd="$scriptsPath/passwd_gen.py"
-alias percentage="$scriptsPath/perc_calc.py"
-alias random="$scriptsPath/randomChoose.py"
-alias tableStyle="$scriptsPath/tableStyle.py"
+alias converterMetric="python3 $scriptsPath/measure_unit_converter.py"
+alias pswd="python3 $scriptsPath/passwd_gen.py"
+alias percentage="python3 $scriptsPath/perc_calc.py"
+alias random="python3 $scriptsPath/randomChoose.py"
+alias tableStyle="python3 $scriptsPath/tableStyle.py"
 
 
 
--- Utilities/setup.sh
+++ Utilities/setup.sh
@@ -5,7 +5,7 @@ git config --global user.email "thealldedfred@gmail.com"
 git config --global user.name "Fred-ITUX"
 
 
-find ~/ -type f -name "*.sh" -exec chmod +x {} +
+find ~/phoneScripts -type f -name "*.sh" -exec chmod +x {} +
 
 
 cp ~/phoneScripts/Utilities/profile.sh     ~/.profile
