ca524253d769c7fd22a892a5c62a371eaf72b104
Fred-ITUX <thealldedfred@gmail.com>
2026-02-24 08:25:03 +0100

    Automatic patch update

--- Utilities/profile.sh
+++ Utilities/profile.sh
@@ -27,7 +27,7 @@ alias setup="$utilitiesPath/setup.sh"
 
 
 #### Python scripts
-alias converterMetric="python3 $scriptsPath/measure_unit_converter.py"
+alias convMetric="python3 $scriptsPath/measure_unit_converter.py"
 alias pswd="python3 $scriptsPath/passwd_gen.py"
 alias percentage="python3 $scriptsPath/perc_calc.py"
 alias random="python3 $scriptsPath/randomChoose.py"
