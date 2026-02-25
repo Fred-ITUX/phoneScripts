e2d31554b8762490aece849b9b5de0a7d2a529f9
Fred-ITUX <thealldedfred@gmail.com>
2026-02-25 22:35:36 +0100

    Automatic patch update

---
+++ Utilities/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+git config --global user.email "thealldedfred@gmail.com"                                      
+git config --global user.name "Fred-ITUX"
+
+
+find ~/phoneScripts -type f -name "*.sh" -exec chmod +x {} +
+
+
+cp ~/phoneScripts/Utilities/profile.sh     ~/.profile
+source ~/.profile
---
+++ Utilities/welcome_home.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#### /etc/motd
+
+Welcome to Alpine! (edit /etc/motd)
+
+Install pkg: apk add <package>
+Aliases: ~/.profile
+Keep shell active: loop (alias)
